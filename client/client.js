71ba848f74dfc038a947cc8398d0f3355aacc040
Yun <broody@gmail.com>
2015-05-09 18:45:59 -0700

    Replace cap_pic with real binary for Edison

--- client/client.js
+++ client/client.js
@@ -2,28 +2,28 @@ var request = require('request');
 var fs = require('fs');
 var ifs = require('./app/ifs');
 
-
-ifs.capturePic(function(img) {
-
-    request({
-        url: "http://gcloud.abovethought.com:5000/event",
-        method: "GET"},
-        function(err, res, body) {
-            console.log(body);
-        }
-    );
+setInterval(function() {
+    ifs.capturePic(function(img) {
     /*
-	request({
-        url: "http://gcloud.abovethought.com:5000/event",
-        method: "POST",
-        json: {
-            image: img,
-            temp: 100
-        } },
-        function(err, res, body) {
-			console.log(body);
-        }
-    );
-*/
-
-});
+        request({
+            url: "http://gcloud.abovethought.com:5000/event",
+            method: "GET"},
+            function(err, res, body) {
+                console.log(body);
+            }
+        );
+        */
+        request({
+            url: "http://gcloud.abovethought.com:5000/event",
+            method: "POST",
+            json: {
+                image: img,
+                temp: 100
+            } },
+            function(err, res, body) {
+                console.log(body);
+            }
+        );
+    });
+}, 5000);
+    
